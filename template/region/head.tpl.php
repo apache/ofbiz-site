936a14fc74b91105fa3f963f273a2084928bccd6
Sharan Foga <sharan@apache.org>
2017-08-29 14:30:41 +0000

    Improved:Changed apple-touch-icon favicons to new ofbiz icons and regenerated all website pages
    git-svn-id: http://svn.apache.org/repos/asf/ofbiz/site@1806597 13f79535-47bb-0310-9956-ffa450edef68

--- template/region/head.tpl.php
+++ template/region/head.tpl.php
@@ -47,7 +47,7 @@
 <!-- Favicons
     ================================================== -->
 <link rel="shortcut icon" href="images/favicon.ico">
-<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
-<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="144x144" href="images/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" href="images/apple-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="images/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="images/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="144x144" href="images/apple-icon-144x144.png">
