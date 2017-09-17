1938959f60734a2b0352ea0dc5345f02e328942a
Taher Alkhateeb <taher@apache.org>
2017-09-17 20:33:25 +0000

    Improved: applied a simpler implementation for php2html
    Loop over all page templates and generate them instead of the copy-paste
    pattern and hardcoding the name of each template into the script.
    
    
    git-svn-id: http://svn.apache.org/repos/asf/ofbiz/site@1808651 13f79535-47bb-0310-9956-ffa450edef68

--- php2html.sh
+++ php2html.sh
@@ -19,18 +19,8 @@
 #####################################################################
 
 # This shell script creates the site HTML files from the PHP templates
-php template/html.tpl.php template/page/about-ofbiz.tpl.php > about-ofbiz.html
-php template/html.tpl.php template/page/business-users.tpl.php >business-users.html
-php template/html.tpl.php template/page/developers.tpl.php > developers.html
-php template/html.tpl.php template/page/download.tpl.php > download.html
-php template/html.tpl.php template/page/release-notes-16.11.03.tpl.php > release-notes-16.11.03.html
-php template/html.tpl.php template/page/release-notes-13.07.03.tpl.php > release-notes-13.07.03.html
-php template/html.tpl.php template/page/faqs.tpl.php > faqs.html
-php template/html.tpl.php template/page/getting-involved.tpl.php > getting-involved.html
-php template/html.tpl.php template/page/index.tpl.php > index.html
-php template/html.tpl.php template/page/mailing-lists.tpl.php > mailing-lists.html
-php template/html.tpl.php template/page/ofbiz-demos.tpl.php > ofbiz-demos.html
-php template/html.tpl.php template/page/our-users.tpl.php >our-users.html
-php template/html.tpl.php template/page/service-providers.tpl.php > service-providers.html
-php template/html.tpl.php template/page/source-repositories.tpl.php > source-repositories.html
-php template/html.tpl.php template/page/user-stories.tpl.php > user-stories.html
+SITE_DIR="$(cd -P "$(dirname "$0")" && pwd)"
+for template in $SITE_DIR/template/page/*.tpl.php; do
+    filename=$(basename $template)
+    php $SITE_DIR/template/html.tpl.php $template > $SITE_DIR/${filename%.tpl.php}.html
+done
