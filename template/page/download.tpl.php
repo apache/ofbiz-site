c1abbfc8b1d9cd4e30bb85b78e2555960151b3bb
Michael Brohl <mbrohl@apache.org>
2018-01-05 17:22:34 +0000

    Added https protocol prefix for external links, fixes #OFBIZ-10140
    git-svn-id: http://svn.apache.org/repos/asf/ofbiz/site@1820326 13f79535-47bb-0310-9956-ffa450edef68

--- template/page/download.tpl.php
+++ template/page/download.tpl.php
@@ -31,7 +31,7 @@
                   <ul>
                     <li>
                       <h2>OFBiz 16.11.04</h2>
-                      <a href="//www.apache.org/dyn/closer.lua/ofbiz/apache-ofbiz-16.11.04.zip" target="external" class="moreLink">&rarr; Download</a> </li>
+                      <a href="https://www.apache.org/dyn/closer.lua/ofbiz/apache-ofbiz-16.11.04.zip" target="external" class="moreLink">&rarr; Download</a> </li>
                      </ul>
                 </div>
                 <div class="tab-pane" id="tabs-2">
@@ -55,37 +55,37 @@
             <h2>Apache OFBiz 16.11.04</h2>
             <div class="divider"><span></span></div>
             <p> Released in January 2018, this is the fourth release of the 16.11 series, that has been stabilized since November 2016. </p>
-            <a href ="//www.apache.org/dyn/closer.lua/ofbiz/apache-ofbiz-16.11.04.zip" target="external" >Download OFBiz 16.11.04</a>
-            <a href ="//www.apache.org/dist/ofbiz/apache-ofbiz-16.11.04.zip.asc" target="external">[PGP]</a>
-      <a href ="//www.apache.org/dist/ofbiz/apache-ofbiz-16.11.04.zip.md5" target="external">[MD5]</a>
-            <a href ="//www.apache.org/dist/ofbiz/apache-ofbiz-16.11.04.zip.sha" target="external">[SHA12]</a>
+            <a href ="https://www.apache.org/dyn/closer.lua/ofbiz/apache-ofbiz-16.11.04.zip" target="external" >Download OFBiz 16.11.04</a>
+            <a href ="https://www.apache.org/dist/ofbiz/apache-ofbiz-16.11.04.zip.asc" target="external">[PGP]</a>
+            <a href ="https://www.apache.org/dist/ofbiz/apache-ofbiz-16.11.04.zip.md5" target="external">[MD5]</a>
+            <a href ="https://www.apache.org/dist/ofbiz/apache-ofbiz-16.11.04.zip.sha" target="external">[SHA12]</a>
             <a href ="release-notes-16.11.04.html">[Release Notes]</a></p>
 
             <h2>Earlier Releases</h2>
             <div class="divider"><span></span></div>
-            <p>Older superseded releases of Apache OFBiz can be found in the <a href="//archive.apache.org/dist/ofbiz/" target="external">Apache OFBiz archive</a></p>
+            <p>Older superseded releases of Apache OFBiz can be found in the <a href="https://archive.apache.org/dist/ofbiz/" target="external">Apache OFBiz archive</a></p>
             <p><strong>NOTE: To avoid any security vulnerabilities the Apache OFBiz community highly recommend that all users upgrade to the latest stable release.</strong></p>
-      <p> A descriptions of each release in the history of OFBiz can be <a href="//www.apache.org/dist/ofbiz/" target="external">found here</a></p>
+      <p> A descriptions of each release in the history of OFBiz can be <a href="https://www.apache.org/dist/ofbiz/" target="external">found here</a></p>
       <h2><a id="security"></a>Security Vulnerabilities</h2>
             <div class="divider"><span></span></div>
             <p> <strong> We strongly encourage OfBiz users to report security problems affecting OFBiz to the private security mailing lists (either security@ofbiz.apache.org or security@apache.org), before disclosing them in a public forum.</strong></p>
-<p>Please see the  <a href="//www.apache.org/security/" target="external">ASF Security Team webpage</a> for further information about reporting a security vulnerability as well as their contact information. </p>
+<p>Please see the  <a href="https://www.apache.org/security/" target="external">ASF Security Team webpage</a> for further information about reporting a security vulnerability as well as their contact information. </p>
  
             <h3>List of Known Vulnerabilities</h3>
             <ul class="iconsList">
-        <li><i class="icon-pin"></i> <a href="//cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2017-15714" target="external">CVE-2017-15714</a>; affected releases: from 16.11.01 to 16.11.03; fixed in 16.11.04 with revision </li><a href="//svn.apache.org/viewvc?view=revision&revision=1818482" target="external">1759065</a>
-        <li><i class="icon-pin"></i> <a href="//cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2016-6800" target="external">CVE-2016-6800</a>; affected releases: 13.07.*, 12.04.*, 11.04.*; fixed in 16.11.01 with revisions </li><a href="//svn.apache.org/viewvc?view=revision&revision=1759065" target="external">1759065</a> and <a href="//svn.apache.org/viewvc?view=revision&revision=1759218" target="external"> 1759218</a>
-        <li><i class="icon-pin"></i> <a href="//cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2016-4462" target="external">CVE-2016-4462</a>; affected releases: 13.07.*, 12.04.*, 11.04.*; fixed in 16.11.01 with revisions</li><a href="//svn.apache.org/viewvc?view=revision&revision=1761978" target="external">1761978</a>, </li><a href="//svn.apache.org/viewvc?view=revision&revision=1761986" target="external">1761986</a> and <a href="//svn.apache.org/viewvc?view=revision&revision=1761987" target="external"> 1761987</a>
-        <li><i class="icon-pin"></i> <a href="//cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2016-2170" target="external">CVE-2016-2170</a>; affected releases: 13.07.02 and earlier versions (13.07.*), 12.04.05 and earlier versions (12.04.*); fixed in 13.07.03 and 12.04.06</li>
-        <li><i class="icon-pin"></i> <a href="//cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2015-3268" target="external">CVE-2015-3268</a>; affected releases: 13.07.02 and earlier versions (13.07.*), 12.04.05 and earlier versions (12.04.*); fixed in 13.07.03 and 12.04.06</li>
-        <li><i class="icon-pin"></i> <a href="//cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-0232" target="external">CVE-2014-0232</a>; affected releases: 12.04.03 and earlier versions (12.04.*), 11.04.04 and earlier versions (11.04.*); fixed in 12.04.04 and 11.04.05</li>
-        <li><i class="icon-pin"></i> <a href="//cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2013-2250" target="external">CVE-2013-2250</a>; affected releases: 12.04.01, 11.04.02 and earlier versions (11.04.*), 10.04.05 and earlier versions (10.04.*); fixed in 12.04.02, 11.04.03 and 10.04.06</li>
-        <li><i class="icon-pin"></i> <a href="//cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2013-2137" target="external">CVE-2013-2137</a>; affected releases: 12.04.01, 11.04.02 and earlier versions (11.04.*), 10.04.05 and earlier versions (10.04.*); fixed in 12.04.02, 11.04.03 and 10.04.06</li>
-        <li><i class="icon-pin"></i> <a href="//cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2013-0177" target="external">CVE-2013-0177</a>; affected releases: 11.04.01, 10.04.04 and earlier versions (10.04.*); fixed in 11.04.02 and 10.04.05</li>
-        <li><i class="icon-pin"></i> <a href="//cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2012-3506" target="external">CVE-2012-3506</a>; affected releases: 10.04.02, 10.04 (10.04.01); fixed in 10.04.03</li>
-        <li><i class="icon-pin"></i> <a href="//cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2012-1622" target="external">CVE-2012-1622</a>; affected releases: 10.04 (10.04.01); fixed in 10.04.02</li>
-        <li><i class="icon-pin"></i> <a href="//cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2012-1621" target="external">CVE-2012-1621</a>; affected releases: 10.04 (10.04.01); fixed in 10.04.02</li>
-        <li><i class="icon-pin"></i> <a href="//cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2010-0432" target="external">CVE-2010-0432</a>; affected releases: 09.04; fixed in 09.04.01</li>
+        <li><i class="icon-pin"></i> <a href="https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2017-15714" target="external">CVE-2017-15714</a>; affected releases: from 16.11.01 to 16.11.03; fixed in 16.11.04 with revision </li><a href="https://svn.apache.org/viewvc?view=revision&revision=1818482" target="external">1759065</a>
+        <li><i class="icon-pin"></i> <a href="https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2016-6800" target="external">CVE-2016-6800</a>; affected releases: 13.07.*, 12.04.*, 11.04.*; fixed in 16.11.01 with revisions </li><a href="https://svn.apache.org/viewvc?view=revision&revision=1759065" target="external">1759065</a> and <a href="https://svn.apache.org/viewvc?view=revision&revision=1759218" target="external"> 1759218</a>
+        <li><i class="icon-pin"></i> <a href="https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2016-4462" target="external">CVE-2016-4462</a>; affected releases: 13.07.*, 12.04.*, 11.04.*; fixed in 16.11.01 with revisions</li><a href="https://svn.apache.org/viewvc?view=revision&revision=1761978" target="external">1761978</a>, </li><a href="https://svn.apache.org/viewvc?view=revision&revision=1761986" target="external">1761986</a> and <a href="https://svn.apache.org/viewvc?view=revision&revision=1761987" target="external"> 1761987</a>
+        <li><i class="icon-pin"></i> <a href="https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2016-2170" target="external">CVE-2016-2170</a>; affected releases: 13.07.02 and earlier versions (13.07.*), 12.04.05 and earlier versions (12.04.*); fixed in 13.07.03 and 12.04.06</li>
+        <li><i class="icon-pin"></i> <a href="https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2015-3268" target="external">CVE-2015-3268</a>; affected releases: 13.07.02 and earlier versions (13.07.*), 12.04.05 and earlier versions (12.04.*); fixed in 13.07.03 and 12.04.06</li>
+        <li><i class="icon-pin"></i> <a href="https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-0232" target="external">CVE-2014-0232</a>; affected releases: 12.04.03 and earlier versions (12.04.*), 11.04.04 and earlier versions (11.04.*); fixed in 12.04.04 and 11.04.05</li>
+        <li><i class="icon-pin"></i> <a href="https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2013-2250" target="external">CVE-2013-2250</a>; affected releases: 12.04.01, 11.04.02 and earlier versions (11.04.*), 10.04.05 and earlier versions (10.04.*); fixed in 12.04.02, 11.04.03 and 10.04.06</li>
+        <li><i class="icon-pin"></i> <a href="https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2013-2137" target="external">CVE-2013-2137</a>; affected releases: 12.04.01, 11.04.02 and earlier versions (11.04.*), 10.04.05 and earlier versions (10.04.*); fixed in 12.04.02, 11.04.03 and 10.04.06</li>
+        <li><i class="icon-pin"></i> <a href="https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2013-0177" target="external">CVE-2013-0177</a>; affected releases: 11.04.01, 10.04.04 and earlier versions (10.04.*); fixed in 11.04.02 and 10.04.05</li>
+        <li><i class="icon-pin"></i> <a href="https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2012-3506" target="external">CVE-2012-3506</a>; affected releases: 10.04.02, 10.04 (10.04.01); fixed in 10.04.03</li>
+        <li><i class="icon-pin"></i> <a href="https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2012-1622" target="external">CVE-2012-1622</a>; affected releases: 10.04 (10.04.01); fixed in 10.04.02</li>
+        <li><i class="icon-pin"></i> <a href="https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2012-1621" target="external">CVE-2012-1621</a>; affected releases: 10.04 (10.04.01); fixed in 10.04.02</li>
+        <li><i class="icon-pin"></i> <a href="https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2010-0432" target="external">CVE-2010-0432</a>; affected releases: 09.04; fixed in 09.04.01</li>
             </ul>
         </div>
         </div>
