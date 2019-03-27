88a72310c97d1c34a1c0489c90109b7635e3176b
Deepak Dixit <deepak@apache.org>
2019-03-27 07:40:11 +0000

    Updated the js/css/images path, if you hit the wrong url like https://ofbiz.apache.org/mailing-lists/test ofbiz site fails to laod the resources.
    git-svn-id: http://svn.apache.org/repos/asf/ofbiz/site@1856372 13f79535-47bb-0310-9956-ffa450edef68

--- template/region/head.tpl.php
+++ template/region/head.tpl.php
@@ -15,26 +15,26 @@
 <!-- CSS
     ================================================== -->
 <!-- Bootstrap  -->
-<link type="text/css" rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+<link type="text/css" rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
 <!-- web font  -->
 <link href="//fonts.googleapis.com/css?family=Open+Sans:400,300,800" rel="stylesheet" type="text/css">
 <!-- plugin css  -->
-<link rel="stylesheet" type="text/css" href="js/plugins/pretty-photo/css/prettyPhoto.css" />
-<link rel="stylesheet" type="text/css" href="js/plugins/rs-plugin/css/settings.css" media="screen" />
-<link type="text/css" rel="stylesheet" href="js/plugins/hoverdir/css/style.css">
+<link rel="stylesheet" type="text/css" href="/js/plugins/pretty-photo/css/prettyPhoto.css" />
+<link rel="stylesheet" type="text/css" href="/js/plugins/rs-plugin/css/settings.css" media="screen" />
+<link type="text/css" rel="stylesheet" href="/js/plugins/hoverdir/css/style.css">
 <!-- icon fonts -->
-<link type="text/css" rel="stylesheet" href="font-icons/custom-icons/css/custom-icons.css">
-<link type="text/css" rel="stylesheet" href="font-icons/custom-icons/css/custom-icons-ie7.css">
+<link type="text/css" rel="stylesheet" href="/font-icons/custom-icons/css/custom-icons.css">
+<link type="text/css" rel="stylesheet" href="/font-icons/custom-icons/css/custom-icons-ie7.css">
 <!-- Custom css -->
-<link type="text/css" rel="stylesheet" href="css/layout.css">
-<link type="text/css" id="colors" rel="stylesheet" href="css/colors.css">
+<link type="text/css" rel="stylesheet" href="/css/layout.css">
+<link type="text/css" id="colors" rel="stylesheet" href="/css/colors.css">
 <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 <!--[if gte IE 9]><style type="text/css">.iconBig, .active, .hover a , .Shover a { filter: none !important; } </style> <![endif]-->
 <script src="js/modernizr-2.6.1.min.js"></script>
 <!-- Favicons
     ================================================== -->
-<link rel="shortcut icon" href="images/favicon.ico">
-<link rel="apple-touch-icon" href="images/apple-icon.png">
-<link rel="apple-touch-icon" sizes="72x72" href="images/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="114x114" href="images/apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="144x144" href="images/apple-icon-144x144.png">
+<link rel="shortcut icon" href="/images/favicon.ico">
+<link rel="apple-touch-icon" href="/images/apple-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/images/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/images/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/images/apple-icon-144x144.png">
