a611e0ee45288127a3076dbf4e5e3c8122c85cb7
Jacques Le Roux <jacques.le.roux@les7arts.com>
2020-05-01 11:46:47 +0200

    Improved: Update pages of ofbiz website
    (OFBIZ-11269)
    
    Updates the source-repositories page
    I missed the html page (I guess did not run php2html script)
    
    Thanks: Wiebke Pätzold for the patch, Pierre Smits and Pawan for review

--- template/page/source-repositories.tpl.php
+++ template/page/source-repositories.tpl.php
@@ -1,7 +1,7 @@
 <?php //Variable declarations for region templates
   $head_title = '<title>The Apache OFBiz&reg; Project - Source Repository</title>';
 ?>
-
+   
   <!-- page content -->
  <section id="content" class="fullWidth">
     <header class="headerPage">
@@ -27,69 +27,58 @@
               <div class="span10">
                 <h2>Public Access</h2>
                 <div class="divider"><span></span></div>
-    <p>Anyone can checkout or browse the source code in the OFBiz gitbox repository.</P>
+    <p>Anyone can checkout or browse the source code in the OFBiz public Subversion (SVN) repository.</P>
     <h3>Browsing the Repository</h3>
                 <div class="divider"><span></span></div>
                 <p>You can browse the repository using any of the following links.</p>
                 <ul class="iconsList">
-                   <li><i class="icon-pin"></i> <a href="https://gitbox.apache.org/repos/asf/ofbiz-framework.git" target="external"><strong>gitbox ofbiz-framework</strong></a></li>
-        <li><i class="icon-pin"></i> <a href="https://gitbox.apache.org/repos/asf/ofbiz-plugins.git" target="external"><strong>gitbox ofbiz-plugins</strong></a></li>
-        <li><i class="icon-pin"></i> <a href="https://gitbox.apache.org/repos/asf/ofbiz-site.git" target="external"><strong>gitbox ofbiz-site</strong></a></li>
-        <li><i class="icon-pin"></i> <a href="https://gitbox.apache.org/repos/asf/ofbiz-tools.git" target="external"><strong>gitbox ofbiz-tools</strong></a></li>
+                   <li><i class="icon-pin"></i> <a href="//svn.apache.org/repos/asf/ofbiz/" target="external"><strong>SVN - WebDAV</strong></a></li>
+        <li><i class="icon-pin"></i> <a href="//svn.apache.org/viewvc/ofbiz/" target="external"><strong>SVN - ViewVC</strong></a></li>
     </ul>
     <p></p>
     <p>Our ofbiz-framework trunk and ofbiz-plugins trunk are also available on Git at the links below:</p>
     <ul class="iconsList">
-        <li><i class="icon-pin"></i> <a href="https://github.com/apache/ofbiz-framework" target="external"><strong>ofbiz-framework trunk on Github</strong></a></li>
-       <li><i class="icon-pin"></i> <a href="https://github.com/apache/ofbiz-plugins" target="external"><strong>ofbiz-plugins trunk on Github</strong></a></li>
+        <li><i class="icon-pin"></i> <a href="//github.com/apache/ofbiz-framework" target="external"><strong>ofbiz-framework trunk on Github</strong></a></li>
+       <li><i class="icon-pin"></i> <a href="//github.com/apache/ofbiz-plugins" target="external"><strong>ofbiz-plugins trunk on Github</strong></a></li>
     </ul>
 
     <h3>Checking out the Repository Source Code</h3>
-    <p><strong>NOTE: </strong> Apache OFBiz uses <a href="//git.apache.org/" target="external"><strong>Git</strong></a> for version control of our source repository. If you are not familiar with Git and you don't have a Git client tool, then the following could be useful:</p>
-    <p><i class="icon-pin"></i><a href="//git.apache.org/" target="external"><strong>ASF Writable Git Services</strong></a></p>
+    <p><strong>NOTE: </strong> Apache OFBiz uses <a href="//subversion.apache.org/" target="external"><strong>Apache Subversion</strong></a> for version control of our source repository. If you are not familiar with Apache Subversion and you don't have an SVN client tool, then following links could be useful:</p>
+    <p><i class="icon-pin"></i><a href="//subversion.apache.org/packages.html" target="external"><strong>SVN Client Tools</strong></a></p>
+    <p><i class="icon-pin"></i><a href="//subversion.apache.org/docs/" target="external"><strong>SVN Documenentation</strong></a></p>
     <p>To checkout the source code, simply use the following command (if you are using a GUI client, configure it appropriately).</p>
     <h2>Trunk</h2>
-    <p>On gitbox:</p>
      <dl class="iconsList">
         <dt><i class="icon-pin"></i> <strong>ofbiz-framework</strong></dt>
-        <dd><code>$ git clone https://gitbox.apache.org/repos/asf/ofbiz-framework.git ofbiz-framework </code></dd>
+        <dd><code>$ svn co http://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/trunk ofbiz-framework</code></dd>
         <dt><i class="icon-pin"></i> <strong>ofbiz-plugins</strong></dt>
-        <dd><code>git clone https://gitbox.apache.org/repos/asf/ofbiz-plugins.git plugins </code></dd>
-    </dl>
-    <p>On GitHub:</p>
-     <dl class="iconsList">
-        <dt><i class="icon-pin"></i> <strong>ofbiz-framework</strong></dt>
-        <dd><code>$ git clone https://github.com/apache/ofbiz-framework.git ofbiz-framework </code></dd>
-        <dt><i class="icon-pin"></i> <strong>ofbiz-plugins</strong></dt>
-        <dd><code>$ git clone https://github.com/apache/ofbiz-plugins.git plugins </code></dd>
+        <dd><code>$ svn co http://svn.apache.org/repos/asf/ofbiz/ofbiz-plugins/trunk ofbiz-plugins </code></dd>
     </dl>
     <h2>Releases</h2>
     <dl class="iconsList">
-        <dt><i class="icon-pin"></i> <strong>ofbiz-framework branch release17.12 (stable)</strong><dt>
-        <dd><code>$ git clone https://gitbox.apache.org/repos/asf/ofbiz-framework.git ofbiz-framework </code></dd>
-        <dd><code>$ git checkout release17.12.01</code></dd>
-        <dt><i class="icon-pin"></i> <strong>ofbiz-plugins branch release17.12 (stable)</strong><dt>
-        <dd><code>$ git clone https://gitbox.apache.org/repos/asf/ofbiz-plugins.git plugins </code></dd>
-        <dd><code>$ git checkout release17.12.01</code></dd>
         <dt><i class="icon-pin"></i> <strong>ofbiz-framework branch release18.12 </strong></dt>
-        <dd><code>$ git clone https://gitbox.apache.org/repos/asf/ofbiz-framework.git ofbiz-framework </code></dd>
-        <dd><code>$ git checkout origin/release18.12</code></dd>
-        <dt><i class="icon-pin"></i> <strong>ofbiz-plugins branch release18.12 </strong><dt>
-        <dd><code>$ git clone https://gitbox.apache.org/repos/asf/ofbiz-plugins.git plugins </code></dd>
-        <dd><code>$ git checkout origin/release18.12 </code></dd>
+        <dd><code>$ svn co http://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/branches/release18.12 ofbiz.18.12</code></dd>
+        <dt><i class="icon-pin"></i> <strong>ofbiz-plugins branch release18.12 </strong></dt>
+        <dd><code>$ svn co http://svn.apache.org/repos/asf/ofbiz/ofbiz-plugins/branches/release18.12 ofbiz.18.12-plugins</code></dd>
+        <dt><i class="icon-pin"></i> <strong>ofbiz-framework branch release17.12 </strong></dt>
+        <dd><code>$ svn co http://svn.apache.org/repos/asf/ofbiz/ofbiz-framework/branches/release17.12 ofbiz.17.12</code></dd>
+        <dt><i class="icon-pin"></i> <strong>ofbiz-plugins branch release17.12 </strong></dt>
+        <dd><code>$ svn co http://svn.apache.org/repos/asf/ofbiz/ofbiz-plugins/branches/release17.12 ofbiz.17.12-plugins</code></dd>
+        <dt><i class="icon-pin"></i> <strong>branch release16.11 (stable)</strong><dt>
+        <dd><code>$ svn co http://svn.apache.org/repos/asf/ofbiz/branches/release16.11 ofbiz.16.11</code></dd>
     </dl>
     <h2>Tools</h2>
     <dl class="iconsList">
       <dt><i class="icon-pin"></i> <strong>ofbiz tools</strong></dt>
-      <dd><code>$ git clone https://gitbox.apache.org/repos/asf/ofbiz-tools.git ofbiz-tools </code></dd>
+      <dd><code>$ svn co http://svn.apache.org/repos/asf/ofbiz/tools ofbiz-tools </code></dd>
     </dl>
     <h2>Website</h2>
     <dl class="iconsList">
       <dt><i class="icon-pin"></i> <strong>ofbiz website</strong></dt>
-      <dd><code>$ git clone https://gitbox.apache.org/repos/asf/ofbiz-site.git ofbiz-website </code></dd>
+      <dd><code>$ svn co http://svn.apache.org/repos/asf/ofbiz/site ofbiz-website </code></dd>
     </dl>
     </div>
-            </section>
+            </section> 
 
       <section  id="SRepoComm" class="slice row clearfix">
               <div class="span10">
