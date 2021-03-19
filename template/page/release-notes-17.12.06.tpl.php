68ff40318a96296af767a7d599126641cb224154
Jacopo Cappellato <jacopo.cappellato@gmail.com>
2021-03-19 14:31:33 +0100

    Add release notes and info about the new release 17.12.06.

---
+++ template/page/release-notes-17.12.06.tpl.php
@@ -0,0 +1,72 @@
+17.12.06<?php //Variable declarations for region templates
+  $head_title = '<title>The Apache OFBiz&reg; Project - Release Notes 17.12.06</title>';
+?>
+ <!-- page content -->
+ <section id="content" class="fullWidth">
+    <header class="headerPage">
+      <div class="container clearfix">
+        <div class="row">
+          <h1 class="span8">Release Notes 17.12.06</h1>
+      <div class="span4" id="navTrail"> <a href="index.html" class="homeLink">home</a><span>/</span><a href="download.html">Download</a><span>/</span><span class="current">Release Notes 17.12.06</span> </div>
+        </div>
+      </div>
+    </header>
+  <section id="content" class="features" >
+  <div class="slice clearfix">
+      <div class="container">
+<div class="row">
+  <div>
+  <p>Apache OFBiz&reg; 17.12.06, released on March 2021, is the sixth and final release of the 17.12 series, that has been stabilized since December 2017.</p>
+
+    
+<h2>        Sub-task
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12165'>OFBIZ-12165</a>] -         Upgrade Tomcat from 9.0.41 to 9.0.43
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12195'>OFBIZ-12195</a>] -         webtools/control/threadList no longer works on trunk (only)
+</li>
+</ul>
+            
+<h2>        Bug
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-10308'>OFBIZ-10308</a>] -         Financial account transaction --&gt; accounting transaction
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-10426'>OFBIZ-10426</a>] -         When creating blog or forums, it failed to save
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-10791'>OFBIZ-10791</a>] -         Unable to add product (of ASSET_USAGE type) in order
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-11221'>OFBIZ-11221</a>] -         Wrong heading on creating quote under order component.
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-11680'>OFBIZ-11680</a>] -         ViewQuoteProfit shows fields in wrong order
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12110'>OFBIZ-12110</a>] -         Overlapped labels in Accounting transaction pdf export
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12153'>OFBIZ-12153</a>] -         Error in deleting Financial Account
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12173'>OFBIZ-12173</a>] -         Visit disabling causes NullPointerException on ecomm groovy script
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12182'>OFBIZ-12182</a>] -         Bypass Java GString to String conversion in ObjectType::simpleTypeOrObjectConvert
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12187'>OFBIZ-12187</a>] -         Error while running MRP
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12189'>OFBIZ-12189</a>] -         Exception error on ecommerce portal while trying to register new user
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12191'>OFBIZ-12191</a>] -         Bug preventing proper explosion of BOM containing virtual nodes
+</li>
+</ul>
+                
+<h2>        Improvement
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12181'>OFBIZ-12181</a>] -         Enlarge the &quot;more-app&quot; menu in Rainbow Stome theme
+</li>
+</ul>  
+
+  </div>
+</div>
+</div>
+</div>
+</section>
+</section>
