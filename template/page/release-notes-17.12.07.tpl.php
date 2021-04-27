0649e348648671c6894ab3b8c746495275577c19
Jacopo Cappellato <jacopo.cappellato@gmail.com>
2021-04-27 11:50:01 +0200

    Publish new release information for 17.12.07

---
+++ template/page/release-notes-17.12.07.tpl.php
@@ -0,0 +1,59 @@
+17.12.06<?php //Variable declarations for region templates
+  $head_title = '<title>The Apache OFBiz&reg; Project - Release Notes 17.12.07</title>';
+?>
+ <!-- page content -->
+ <section id="content" class="fullWidth">
+    <header class="headerPage">
+      <div class="container clearfix">
+        <div class="row">
+          <h1 class="span8">Release Notes 17.12.07</h1>
+      <div class="span4" id="navTrail"> <a href="index.html" class="homeLink">home</a><span>/</span><a href="download.html">Download</a><span>/</span><span class="current">Release Notes 17.12.07</span> </div>
+        </div>
+      </div>
+    </header>
+  <section id="content" class="features" >
+  <div class="slice clearfix">
+      <div class="container">
+<div class="row">
+  <div>
+  <p>Apache OFBiz&reg; 17.12.07, released on April 2021, is the seventh and final release of the 17.12 series, that has been stabilized since December 2017.</p>
+
+    
+<h2>        Sub-task
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12080'>OFBIZ-12080</a>] -         Secure the uploads
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12205'>OFBIZ-12205</a>] -         Upgrade Apache PDFBox to 2.0.23  because of CVE-2021-27807 and CVE-2021-27906
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12212'>OFBIZ-12212</a>] -         Comment out the SOAP and HTTP engines
+</li>
+</ul>
+            
+<h2>        Bug
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12175'>OFBIZ-12175</a>] -         Party Export/Import not congruent
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12211'>OFBIZ-12211</a>] -         addImageForProduct fails
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12222'>OFBIZ-12222</a>] -         Error while deleting a Facility because of an existing FacilityLocationGeoPoint
+</li>
+</ul>
+                
+<h2>        Improvement
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12216'>OFBIZ-12216</a>] -         Improve UtilObject class
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12221'>OFBIZ-12221</a>] -         Improve ObjectInputStream denyList
+</li>
+</ul>
+                                                                                                                                                                
+
+  </div>
+</div>
+</div>
+</div>
+</section>
+</section>
