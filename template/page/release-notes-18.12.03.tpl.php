e14a9fadc31049223df918fa8a395ce141dd546a
Jacopo Cappellato <jacopo.cappellato@gmail.com>
2021-12-12 19:43:14 +0100

    Publish release 18.12.03.

---
+++ template/page/release-notes-18.12.03.tpl.php
@@ -0,0 +1,40 @@
+<?php //Variable declarations for region templates
+  $head_title = '<title>The Apache OFBiz&reg; Project - Release Notes 18.12.03</title>';
+?>
+ <!-- page content -->
+ <section id="content" class="fullWidth">
+    <header class="headerPage">
+      <div class="container clearfix">
+        <div class="row">
+          <h1 class="span8">Release Notes 18.12.03</h1>
+      <div class="span4" id="navTrail"> <a href="index.html" class="homeLink">home</a><span>/</span><a href="download.html">Download</a><span>/</span><span class="current">Release Notes 18.12.03</span> </div>
+        </div>
+      </div>
+    </header>
+  <section id="content" class="features" >
+  <div class="slice clearfix">
+      <div class="container">
+<div class="row">
+  <div>
+  <p>Apache OFBiz&reg; 18.12.03, released on December 2021, is the third release of the 18.12 series, that has been stabilized since December 2018.</p>
+
+<h2>        Sub-task
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12449'>OFBIZ-12449</a>] -         [SECURITY] CVE-2021-44228: Apache Log4j2
+</li>
+</ul>
+            
+<h2>        Bug
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12437'>OFBIZ-12437</a>] -         Resolve local xsd on UtilXml class
+</li>
+</ul>                                                                                                                                                         
+
+  </div>
+</div>
+</div>
+</div>
+</section>
+</section>
