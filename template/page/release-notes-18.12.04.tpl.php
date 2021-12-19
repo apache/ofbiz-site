7be476d33a35190ea863e1d2b9ec7b113371512c
Jacopo Cappellato <jacopo.cappellato@gmail.com>
2021-12-19 11:19:01 +0100

    Publish information about the new release 18.12.04

---
+++ template/page/release-notes-18.12.04.tpl.php
@@ -0,0 +1,44 @@
+<?php //Variable declarations for region templates
+  $head_title = '<title>The Apache OFBiz&reg; Project - Release Notes 18.12.04</title>';
+?>
+ <!-- page content -->
+ <section id="content" class="fullWidth">
+    <header class="headerPage">
+      <div class="container clearfix">
+        <div class="row">
+          <h1 class="span8">Release Notes 18.12.04</h1>
+      <div class="span4" id="navTrail"> <a href="index.html" class="homeLink">home</a><span>/</span><a href="download.html">Download</a><span>/</span><span class="current">Release Notes 18.12.04</span> </div>
+        </div>
+      </div>
+    </header>
+  <section id="content" class="features" >
+  <div class="slice clearfix">
+      <div class="container">
+<div class="row">
+  <div>
+  <p>Apache OFBiz&reg; 18.12.04, released on December 2021, is the fourth release of the 18.12 series, that has been stabilized since December 2018.</p>
+
+
+<h2>        Sub-task
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12464'>OFBIZ-12464</a>] -         Update Solr and Lucene to address several CVEs (including Log4j)
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12470'>OFBIZ-12470</a>] -         [SECURITY] CVE-2021-45105: Apache Log4j2
+</li>
+</ul>
+            
+<h2>        Bug
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12468'>OFBIZ-12468</a>] -         Bug in HtmlFormMacroLibrary.ftl::makeHyperlinkString macro 
+</li>
+</ul>
+
+
+  </div>
+</div>
+</div>
+</div>
+</section>
+</section>
