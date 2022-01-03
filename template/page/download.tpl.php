494ea30a072fe6485dba4e2ed11de9d0df1c5519
Jacopo Cappellato <jacopo.cappellato@gmail.com>
2022-01-03 13:50:25 +0100

    Update the download page template and add the release note template for the new release 18.12.05. Note that I didn't generate the html files on purpose: I will create them tomorrow to publish the pages with the announcement.

--- template/page/download.tpl.php
+++ template/page/download.tpl.php
@@ -30,17 +30,17 @@
                 <div class="tab-pane active" id="tabs-1">
                   <ul>
                     <li>
-                      <h2>OFBiz 18.12.04</h2>
-                      <a href="https://www.apache.org/dyn/closer.lua/ofbiz/apache-ofbiz-18.12.04.zip" target="external" class="moreLink">&rarr; Download</a>
-                    </li>
-                    <li>
-                      <h2>OFBiz 17.12.08</h2>
-                      <a href="https://www.apache.org/dyn/closer.lua/ofbiz/apache-ofbiz-17.12.08.zip" target="external" class="moreLink">&rarr; Download</a>
+                      <h2>OFBiz 18.12.05</h2>
+                      <a href="https://www.apache.org/dyn/closer.lua/ofbiz/apache-ofbiz-18.12.05.zip" target="external" class="moreLink">&rarr; Download</a>
                     </li>
                   </ul>
                 </div>
                 <div class="tab-pane" id="tabs-2">
                   <ul>
+                    <li>
+                      <h2>OFBiz 18.12.05</h2>
+                      <a href="release-notes-18.12.05.html" class="moreLink">&rarr; View</a>
+                    </li>
                     <li>
                       <h2>OFBiz 18.12.04</h2>
                       <a href="release-notes-18.12.04.html" class="moreLink">&rarr; View</a>
@@ -76,14 +76,14 @@
             <p> <strong> Then, to install OFBiz, follow the explanations in the "INSTALL" file found in the just downloaded/extracted OFBiz main directory.</strong> </p>
 
             <p> <strong>PLEASE NOTE:</strong> Despite our best efforts to maintain up to three active release branches, support for older branches can decrease because our project volunteers may be focused on other issues. We recommend using releases from the most recent branch wherever possible. </p>
-            <h2>Apache OFBiz 18.12.04</h2>
+            <h2>Apache OFBiz 18.12.05</h2>
             <div class="divider"><span></span></div>
-            <p> Released on December 2021, this is the fourth release of the 18.12 series, that has been stabilized since December 2018.</p>
-            <a href="https://www.apache.org/dyn/closer.lua/ofbiz/apache-ofbiz-18.12.04.zip" target="external" >Download OFBiz 18.12.04</a>
-            <a href="https://downloads.apache.org/ofbiz/apache-ofbiz-18.12.04.zip.asc" target="external">[PGP]</a>
-            <a href="https://downloads.apache.org/ofbiz/apache-ofbiz-18.12.04.zip.sha512" target="external">[SHA512]</a>
+            <p> Released on January 2022, this is the fifth release of the 18.12 series, that has been stabilized since December 2018.</p>
+            <a href="https://www.apache.org/dyn/closer.lua/ofbiz/apache-ofbiz-18.12.05.zip" target="external" >Download OFBiz 18.12.05</a>
+            <a href="https://downloads.apache.org/ofbiz/apache-ofbiz-18.12.05.zip.asc" target="external">[PGP]</a>
+            <a href="https://downloads.apache.org/ofbiz/apache-ofbiz-18.12.05.zip.sha512" target="external">[SHA512]</a>
             <a href="https://downloads.apache.org/ofbiz/KEYS" target="external">[KEYS]</a>
-            <a href="release-notes-18.12.04.html">[Release Notes]</a>
+            <a href="release-notes-18.12.05.html">[Release Notes]</a>
 
             <h2>Apache OFBiz 17.12.08</h2>
             <div class="divider"><span></span></div>
