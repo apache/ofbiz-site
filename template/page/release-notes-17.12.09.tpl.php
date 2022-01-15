779e5c49e633343f4b323ee538050e945520b8fd
Jacopo Cappellato <jacopo.cappellato@gmail.com>
2022-01-15 13:26:15 +0100

    Fixed wrong release number on the newly created release notes for 17.12.09

--- template/page/release-notes-17.12.09.tpl.php
+++ template/page/release-notes-17.12.09.tpl.php
@@ -1,13 +1,13 @@
 <?php //Variable declarations for region templates
-  $head_title = '<title>The Apache OFBiz&reg; Project - Release Notes 17.12.08</title>';
+  $head_title = '<title>The Apache OFBiz&reg; Project - Release Notes 17.12.09</title>';
 ?>
  <!-- page content -->
  <section id="content" class="fullWidth">
     <header class="headerPage">
       <div class="container clearfix">
         <div class="row">
-          <h1 class="span8">Release Notes 17.12.08</h1>
-      <div class="span4" id="navTrail"> <a href="index.html" class="homeLink">home</a><span>/</span><a href="download.html">Download</a><span>/</span><span class="current">Release Notes 17.12.08</span> </div>
+          <h1 class="span8">Release Notes 17.12.09</h1>
+      <div class="span4" id="navTrail"> <a href="index.html" class="homeLink">home</a><span>/</span><a href="download.html">Download</a><span>/</span><span class="current">Release Notes 17.12.09</span> </div>
         </div>
       </div>
     </header>
