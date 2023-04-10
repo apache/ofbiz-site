b02af9178a534c1d6f5cfdaac82dba8d10dba0c4
Jacopo Cappellato <jacopo.cappellato@gmail.com>
2023-04-10 10:43:13 +0200

    Add release info for the new Apache 18.12.07 release.

--- template/page/download.tpl.php
+++ template/page/download.tpl.php
@@ -30,13 +30,17 @@
                 <div class="tab-pane active" id="tabs-1">
                   <ul>
                     <li>
-                      <h2>OFBiz 18.12.06</h2>
-                      <a href="https://www.apache.org/dyn/closer.lua/ofbiz/apache-ofbiz-18.12.06.zip" target="external" class="moreLink">&rarr; Download</a>
+                      <h2>OFBiz 18.12.07</h2>
+                      <a href="https://www.apache.org/dyn/closer.lua/ofbiz/apache-ofbiz-18.12.07.zip" target="external" class="moreLink">&rarr; Download</a>
                     </li>
                   </ul>
                 </div>
                 <div class="tab-pane" id="tabs-2">
                   <ul>
+                    <li>
+                      <h2>OFBiz 18.12.07</h2>
+                      <a href="release-notes-18.12.07.html" class="moreLink">&rarr; View</a>
+                    </li>
                     <li>
                       <h2>OFBiz 18.12.06</h2>
                       <a href="release-notes-18.12.06.html" class="moreLink">&rarr; View</a>
@@ -157,14 +161,14 @@
             <p>The history of security related fixes included in each release is
 available <a href="security.html">here</a></p>
 
-            <h2>Apache OFBiz 18.12.06</h2>
+            <h2>Apache OFBiz 18.12.07</h2>
             <div class="divider"><span></span></div>
-            <p> Released on September 2022, this is the sixth and final release of the 18.12 series, that has been stabilized since December 2018.</p>
-            <a href="https://www.apache.org/dyn/closer.lua/ofbiz/apache-ofbiz-18.12.06.zip" target="external" >Download OFBiz 18.12.06</a>
-            <a href="https://downloads.apache.org/ofbiz/apache-ofbiz-18.12.06.zip.asc" target="external">[PGP]</a>
-            <a href="https://downloads.apache.org/ofbiz/apache-ofbiz-18.12.06.zip.sha512" target="external">[SHA512]</a>
+            <p> Released on April 2023, this is the seventh and final release of the 18.12 series, that has been stabilized since December 2018.</p>
+            <a href="https://www.apache.org/dyn/closer.lua/ofbiz/apache-ofbiz-18.12.07.zip" target="external" >Download OFBiz 18.12.07</a>
+            <a href="https://downloads.apache.org/ofbiz/apache-ofbiz-18.12.07.zip.asc" target="external">[PGP]</a>
+            <a href="https://downloads.apache.org/ofbiz/apache-ofbiz-18.12.07.zip.sha512" target="external">[SHA512]</a>
             <a href="https://downloads.apache.org/ofbiz/KEYS" target="external">[KEYS]</a>
-            <a href="release-notes-18.12.06.html">[Release Notes]</a>
+            <a href="release-notes-18.12.07.html">[Release Notes]</a>
 
             <p><strong>We strongly encourage OfBiz users to report security problems affecting OFBiz to the private security mailing lists (either security@ofbiz.apache.org or security@apache.org),
              before disclosing them in a public forum. Please don't pack several vulnerabilities in the same report, send them one by one, thanks in advance.</strong></p>
---
+++ template/page/release-notes-18.12.07.tpl.php
@@ -0,0 +1,38 @@
+<?php //Variable declarations for region templates
+  $head_title = '<title>The Apache OFBiz&reg; Project - Release Notes 18.12.04</title>';
+?>
+ <!-- page content -->
+ <section id="content" class="fullWidth">
+    <header class="headerPage">
+      <div class="container clearfix">
+        <div class="row">
+          <h1 class="span8">Release Notes 18.12.07</h1>
+      <div class="span4" id="navTrail"> <a href="index.html" class="homeLink">home</a><span>/</span><a href="download.html">Download</a><span>/</span><span class="current">Release Notes 18.12.07</span> </div>
+        </div>
+      </div>
+    </header>
+  <section id="content" class="features" >
+  <div class="slice clearfix">
+      <div class="container">
+<div class="row">
+  <div>
+  <p>Apache OFBiz&reg; 18.12.07, released on April 2023, is the seventh and final release of the 18.12 series, that has been stabilized since December 2018.</p>
+
+
+
+
+        Release Notes - OFBiz - Version 18.12.07
+                
+<h2>        Bug
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12792'>OFBIZ-12792</a>] -         Disallow unauthorized users to use Solr
+</li>
+</ul> 
+
+  </div>
+</div>
+</div>
+</div>
+</section>
+</section>
