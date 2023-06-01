c29d67b819f266242a753049f49d1af378f3ba39
Jacopo Cappellato <jacopo.cappellato@gmail.com>
2023-06-01 10:24:46 +0200

    Add new release information for Apache OFBiz 18.12.08

---
+++ template/page/release-notes-18.12.08.tpl.php
@@ -0,0 +1,38 @@
+<?php //Variable declarations for region templates
+  $head_title = '<title>The Apache OFBiz&reg; Project - Release Notes 18.12.04</title>';
+?>
+ <!-- page content -->
+ <section id="content" class="fullWidth">
+    <header class="headerPage">
+      <div class="container clearfix">
+        <div class="row">
+          <h1 class="span8">Release Notes 18.12.08</h1>
+      <div class="span4" id="navTrail"> <a href="index.html" class="homeLink">home</a><span>/</span><a href="download.html">Download</a><span>/</span><span class="current">Release Notes 18.12.08</span> </div>
+        </div>
+      </div>
+    </header>
+  <section id="content" class="features" >
+  <div class="slice clearfix">
+      <div class="container">
+<div class="row">
+  <div>
+  <p>Apache OFBiz&reg; 18.12.08, released on June 2023, is the eighth and final release of the 18.12 series, that has been stabilized since December 2018.</p>
+
+
+
+
+        Release Notes - OFBiz - Version 18.12.08
+                
+<h2>        Sub-task
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12824'>OFBIZ-12824</a>] -         Disable the Birt component in all branches (including trunk) because of CVE-2022-25371
+</li>
+</ul>
+
+  </div>
+</div>
+</div>
+</div>
+</section>
+</section>
