bfb64e85bfefa0c89eb434ac5f88a2b2b047d694
Jacopo Cappellato <jacopo.cappellato@gmail.com>
2023-11-05 16:09:46 +0100

    Add information about the new release Apache OFBiz 18.12.09.

---
+++ template/page/release-notes-18.12.09.tpl.php
@@ -0,0 +1,55 @@
+<?php //Variable declarations for region templates
+  $head_title = '<title>The Apache OFBiz&reg; Project - Release Notes 18.12.09</title>';
+?>
+ <!-- page content -->
+ <section id="content" class="fullWidth">
+    <header class="headerPage">
+      <div class="container clearfix">
+        <div class="row">
+          <h1 class="span8">Release Notes 18.12.09</h1>
+      <div class="span4" id="navTrail"> <a href="index.html" class="homeLink">home</a><span>/</span><a href="download.html">Download</a><span>/</span><span class="current">Release Notes 18.12.09</span> </div>
+        </div>
+      </div>
+    </header>
+  <section id="content" class="features" >
+  <div class="slice clearfix">
+      <div class="container">
+<div class="row">
+  <div>
+  <p>Apache OFBiz&reg; 18.12.09, released on November 2023, is the ninth release of the 18.12 series, that has been stabilized since December 2018.</p>
+
+        Release Notes - OFBiz - Version 18.12.09
+    
+<h2>        Sub-task
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12831'>OFBIZ-12831</a>] -         [SECURITY]  CVE-2023-34981 Apache Tomcat
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12839'>OFBIZ-12839</a>] -         [CVE-2023-34478] Apache Shiro, before 1.12.0, is susceptible to a path traversal attack
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12857'>OFBIZ-12857</a>] -         Execution of queries without authentication
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12860'>OFBIZ-12860</a>] -         [SECURITY]  Several CVEs in Apache Tomcat
+</li>
+</ul>
+            
+<h2>        Bug
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-11434'>OFBIZ-11434</a>] -         Forum Articles do not respond to pagination
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12849'>OFBIZ-12849</a>] -         [CVE-2023-41080] Update Tomcat to 9.0.80
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12864'>OFBIZ-12864</a>] -         Issue with OFBiz Job Scheduler and Daylight Saving Time
+</li>
+</ul>
+
+
+
+
+  </div>
+</div>
+</div>
+</div>
+</section>
+</section>
