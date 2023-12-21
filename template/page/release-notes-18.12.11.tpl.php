ae21bcbdc17a45659544fc2dab9c2d55241fbb15
Jacopo Cappellato <jacopo.cappellato@gmail.com>
2023-12-21 18:06:13 +0100

    Add release info for Apache OFBiz 18.12.11.

---
+++ template/page/release-notes-18.12.11.tpl.php
@@ -0,0 +1,36 @@
+<?php //Variable declarations for region templates
+  $head_title = '<title>The Apache OFBiz&reg; Project - Release Notes 18.12.11</title>';
+?>
+ <!-- page content -->
+ <section id="content" class="fullWidth">
+    <header class="headerPage">
+      <div class="container clearfix">
+        <div class="row">
+          <h1 class="span8">Release Notes 18.12.11</h1>
+      <div class="span4" id="navTrail"> <a href="index.html" class="homeLink">home</a><span>/</span><a href="download.html">Download</a><span>/</span><span class="current">Release Notes 18.12.11</span> </div>
+        </div>
+      </div>
+    </header>
+  <section id="content" class="features" >
+  <div class="slice clearfix">
+      <div class="container">
+<div class="row">
+  <div>
+  <p>Apache OFBiz&reg; 18.12.11, released on December 2023, is the eleventh release of the 18.12 series, that has been stabilized since December 2018.</p>
+
+        Release Notes - OFBiz - Version 18.12.11
+                                
+<h2>        Improvement
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12873'>OFBIZ-12873</a>] -         Replaced direct null checks on username, password, and token with UtilValidate.isEmpty() method calls for consistency.
+</li>
+</ul>
+
+
+  </div>
+</div>
+</div>
+</div>
+</section>
+</section>
