2e76f23f71acd00979d91e52cd4aee489dca63c0
Jacopo Cappellato <jacopo.cappellato@gmail.com>
2024-02-28 12:16:47 +0100

    Publish release 18.12.12.

---
+++ template/page/release-notes-18.12.12.tpl.php
@@ -0,0 +1,59 @@
+<?php //Variable declarations for region templates
+  $head_title = '<title>The Apache OFBiz&reg; Project - Release Notes 18.12.12</title>';
+?>
+ <!-- page content -->
+ <section id="content" class="fullWidth">
+    <header class="headerPage">
+      <div class="container clearfix">
+        <div class="row">
+          <h1 class="span8">Release Notes 18.12.12</h1>
+      <div class="span4" id="navTrail"> <a href="index.html" class="homeLink">home</a><span>/</span><a href="download.html">Download</a><span>/</span><span class="current">Release Notes 18.12.12</span> </div>
+        </div>
+      </div>
+    </header>
+  <section id="content" class="features" >
+  <div class="slice clearfix">
+      <div class="container">
+<div class="row">
+  <div>
+  <p>Apache OFBiz&reg; 18.12.12, released on February 2024, is the twelfth release of the 18.12 series, that has been stabilized since December 2018.</p>
+
+        Release Notes - OFBiz - Version 18.12.12
+                                
+<h2>        Bug
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12817'>OFBIZ-12817</a>] -         Pagination: jump to page with AJAX enabled is broken
+</li>
+</ul>
+                
+<h2>        Improvement
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-11839'>OFBIZ-11839</a>] -         Add CHANGELOG.adoc file
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12884'>OFBIZ-12884</a>] -         Don&#39;t need to show files names in UI messages
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12887'>OFBIZ-12887</a>] -         Normalize contextPath in hasBasePermission
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12891'>OFBIZ-12891</a>] -         Screen Communications in Party should not show create trigger to user with VIEW permissions
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12894'>OFBIZ-12894</a>] -         Make loadJWT request auth true
+</li>
+</ul>
+            
+<h2>        Task
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12868'>OFBIZ-12868</a>] -         Replace SvnCheckout in Gradle
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12876'>OFBIZ-12876</a>] -         Use git checkout to retrieve ofbiz plugin sources when building docker images
+</li>
+</ul>
+
+  </div>
+</div>
+</div>
+</div>
+</section>
+</section>
