f2392a6c0c4a27455cdd4344770a238bf932f9bd
Jacopo Cappellato <jacopo.cappellato@gmail.com>
2024-05-07 12:05:25 +0200

    Publish new release information on the website.

---
+++ template/page/release-notes-18.12.13.tpl.php
@@ -0,0 +1,67 @@
+<?php //Variable declarations for region templates
+  $head_title = '<title>The Apache OFBiz&reg; Project - Release Notes 18.12.12</title>';
+?>
+ <!-- page content -->
+ <section id="content" class="fullWidth">
+    <header class="headerPage">
+      <div class="container clearfix">
+        <div class="row">
+          <h1 class="span8">Release Notes 18.12.13</h1>
+      <div class="span4" id="navTrail"> <a href="index.html" class="homeLink">home</a><span>/</span><a href="download.html">Download</a><span>/</span><span class="current">Release Notes 18.12.13</span> </div>
+        </div>
+      </div>
+    </header>
+  <section id="content" class="features" >
+  <div class="slice clearfix">
+      <div class="container">
+<div class="row">
+  <div>
+  <p>Apache OFBiz&reg; 18.12.13, released on May 2024, is the 13th release of the 18.12 series, that has been stabilized since December 2018.</p>
+
+        Release Notes - OFBiz - Version 18.12.13
+                                
+<h2>        Sub-task
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12942'>OFBIZ-12942</a>] -         [SECURITY]  Several CVEs in Apache Tomcat
+</li>
+</ul>
+            
+<h2>        Bug
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-10111'>OFBIZ-10111</a>] -         The child event added is not listed in the Child Work Efforts. While if we add an existing event it is listed in the WorkEffort component
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12820'>OFBIZ-12820</a>] -         PaymentGroup Cancel button is shown to viewer
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12932'>OFBIZ-12932</a>] -         The view log feature in webtools is missing the &quot;Log File Name&quot;
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12936'>OFBIZ-12936</a>] -         ‎SecuredUpload‎::checkMaxLinesLength put in a regression for PDF files
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12937'>OFBIZ-12937</a>] -         Prevent possible file race conditions with SecuredUpload class
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13006'>OFBIZ-13006</a>] -         Reject wrong URLs
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13039'>OFBIZ-13039</a>] -         dateType parameter not existing in &quot;renderDateTimeField&quot; Macro 
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13059'>OFBIZ-13059</a>] -         Old error in showPortalPage screen
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13067'>OFBIZ-13067</a>] -         Error while rendering content [APACHE_OFBIZ_HTML]
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13071'>OFBIZ-13071</a>] -         Could not find PortalPage with portalPageId [] 
+</li>
+</ul>
+                
+<h2>        Improvement
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13073'>OFBIZ-13073</a>] -         Use new Tomcat 9 maxDays Access_Log_Valve Attribute
+</li>
+</ul>
+
+  </div>
+</div>
+</div>
+</div>
+</section>
+</section>
