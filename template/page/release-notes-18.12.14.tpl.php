4daee75aa2390bd45b1c3702a8fb58d4bdc7aef3
Jacopo Cappellato <jacopo.cappellato@gmail.com>
2024-05-31 12:57:19 +0200

    Publish release files for 18.12.14 in the website.

---
+++ template/page/release-notes-18.12.14.tpl.php
@@ -0,0 +1,57 @@
+<?php //Variable declarations for region templates
+  $head_title = '<title>The Apache OFBiz&reg; Project - Release Notes 18.12.14</title>';
+?>
+ <!-- page content -->
+ <section id="content" class="fullWidth">
+    <header class="headerPage">
+      <div class="container clearfix">
+        <div class="row">
+          <h1 class="span8">Release Notes 18.12.14</h1>
+      <div class="span4" id="navTrail"> <a href="index.html" class="homeLink">home</a><span>/</span><a href="download.html">Download</a><span>/</span><span class="current">Release Notes 18.12.14</span> </div>
+        </div>
+      </div>
+    </header>
+  <section id="content" class="features" >
+  <div class="slice clearfix">
+      <div class="container">
+<div class="row">
+  <div>
+  <p>Apache OFBiz&reg; 18.12.14, released on May 2024, is the 14th release of the 18.12 series, that has been stabilized since December 2018.</p>
+
+        Release Notes - OFBiz - Version 18.12.14
+                                
+        <h2>        Sub-task
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-7949'>OFBIZ-7949</a>] -         E-commerce : FTL formatting
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-9804'>OFBIZ-9804</a>] -         Link in verification email for Newsletter gives security error
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13092'>OFBIZ-13092</a>] -         Prevent special encoded characters sequences in URLs
+</li>
+</ul>
+            
+<h2>        Bug
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13075'>OFBIZ-13075</a>] -         ${parameters.SEARCH_STRING}  Failed in TagSearch.ftl[at line 20]
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13081'>OFBIZ-13081</a>] -         GenericEntityException in ProductServicesScript.groovy::countProductView
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13094'>OFBIZ-13094</a>] -         Errors after creating a plugin
+</li>
+</ul>
+                
+<h2>        Improvement
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13061'>OFBIZ-13061</a>] -         Prevents to uselessly clutter the logs up with SetTimeZoneFromBrowser errors
+</li>
+</ul>
+
+  </div>
+</div>
+</div>
+</div>
+</section>
+</section>
