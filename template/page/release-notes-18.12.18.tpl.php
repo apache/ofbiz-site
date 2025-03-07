4975373ce7156b80bbac855a2934915cf65f3435
Jacopo Cappellato <jacopo.cappellato@gmail.com>
2025-03-07 11:01:54 +0100

    Add new release information to the website for Apache OFBiz 18.12.18.

---
+++ template/page/release-notes-18.12.18.tpl.php
@@ -0,0 +1,67 @@
+<?php //Variable declarations for region templates
+  $head_title = '<title>The Apache OFBiz&reg; Project - Release Notes 18.12.16</title>';
+?>
+ <!-- page content -->
+ <section id="content" class="fullWidth">
+    <header class="headerPage">
+      <div class="container clearfix">
+        <div class="row">
+          <h1 class="span8">Release Notes 18.12.16</h1>
+      <div class="span4" id="navTrail"> <a href="index.html" class="homeLink">home</a><span>/</span><a href="download.html">Download</a><span>/</span><span class="current">Release Notes 18.12.15</span> </div>
+        </div>
+      </div>
+    </header>
+  <section id="content" class="features" >
+  <div class="slice clearfix">
+      <div class="container">
+<div class="row">
+  <div>
+  <p>Apache OFBiz&reg; 18.12.18, released in March 2025, is the 18th release of the 18.12 series, which has been feature-frozen since December 2018, receiving only bug fixes.</p>
+
+
+        Release Notes - OFBiz - Version 18.12.18
+    
+<h2>        Sub-task
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-9089'>OFBIZ-9089</a>] -         Unit test case for service - createProductFeatureType
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12080'>OFBIZ-12080</a>] -         Secure the uploads
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13092'>OFBIZ-13092</a>] -         [SECURITY] (CVE-2024-36104) Path traversal leading to RCE
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13147'>OFBIZ-13147</a>] -         Prevent URL parameters manipulation
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13180'>OFBIZ-13180</a>] -         [SECURITY]  Several CVEs in Apache Tomcat
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13192'>OFBIZ-13192</a>] -         Issues when uploading SVG files
+</li>
+</ul>
+            
+<h2>        Bug
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13168'>OFBIZ-13168</a>] -         String to numeric conversion does not support non-breaking spaces
+</li>
+</ul>
+            
+<h2>        New Feature
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13198'>OFBIZ-13198</a>] -         Add the OpenSSF Best Practices Badge
+</li>
+</ul>
+    
+<h2>        Improvement
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13213'>OFBIZ-13213</a>] -         Refactor ControlFilter class without functional changes
+</li>
+</ul>
+                                                                                                                                                        
+  </div>
+</div>
+</div>
+</div>
+</section>
+</section>
