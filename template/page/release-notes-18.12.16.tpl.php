4115036f25978b6f739f636e8dcc905c04a4040f
Jacques Le Roux <jacques.le.roux@les7arts.com>
2025-03-09 16:46:42 +0100

    Fixed: Starting from 18.12.05 web page titles and breadcrumbs are not correct.
    Forgot the breadcrumbs since 18.12.16
    
    Thanks:  Wei Zhang for report

--- template/page/release-notes-18.12.16.tpl.php
+++ template/page/release-notes-18.12.16.tpl.php
@@ -7,7 +7,7 @@
       <div class="container clearfix">
         <div class="row">
           <h1 class="span8">Release Notes 18.12.16</h1>
-      <div class="span4" id="navTrail"> <a href="index.html" class="homeLink">home</a><span>/</span><a href="download.html">Download</a><span>/</span><span class="current">Release Notes 18.12.15</span> </div>
+      <div class="span4" id="navTrail"> <a href="index.html" class="homeLink">home</a><span>/</span><a href="download.html">Download</a><span>/</span><span class="current">Release Notes 18.12.16</span> </div>
         </div>
       </div>
     </header>
@@ -20,14 +20,14 @@
 
 
         Release Notes - OFBiz - Version 18.12.16
-                                
+
 <h2>        Improvement
 </h2>
 <ul>
 <li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13130'>OFBIZ-13130</a>] -         Add permission check for view-maps and change defaults for request-maps
 </li>
 </ul>
-                                                                                                                                                                    
+
 
 
   </div>
