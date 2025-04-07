ddd7ad6db543f33cd43b8db505081448bbefb5f1
Nicolas Malin <nicolas.malin@nereide.fr>
2025-04-07 17:27:30 +0200

    Improved: Publish OFBiz version 24.09.01

---
+++ template/page/release-notes-24.09.01.tpl.php
@@ -0,0 +1,101 @@
+<?php //Variable declarations for region templates
+  $head_title = '<title>The Apache OFBiz&reg; Project - Release Notes 24.09.01</title>';
+?>
+ <!-- page content -->
+ <section id="content" class="fullWidth">
+    <header class="headerPage">
+      <div class="container clearfix">
+        <div class="row">
+          <h1 class="span8">Release Notes 24.09.01</h1>
+      <div class="span4" id="navTrail"> <a href="index.html" class="homeLink">home</a><span>/</span><a href="download.html">Download</a><span>/</span><span class="current">Release Notes 24.09.01</span> </div>
+        </div>
+      </div>
+    </header>
+  <section id="content" class="features" >
+  <div class="slice clearfix">
+      <div class="container">
+<div class="row">
+  <div>
+  <p>Apache OFBiz&reg; 24.09.01, released in April 2025, is the first release of the 24.09 series, which has been feature-frozen since September 2024, receiving only bug fixes.</p>
+
+<h2>Sub-task</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12080'>OFBIZ-12080</a>] - Secure the uploads
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12127'>OFBIZ-12127</a>] - Refactor MacroFormRenderer to use RenderableFtlElements for drop down fields
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13092'>OFBIZ-13092</a>] - [SECURITY] (CVE-2024-36104) Path traversal leading to RCE
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13131'>OFBIZ-13131</a>] - Test 2.3.34 FreeMarker release
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13174'>OFBIZ-13174</a>] - Helveticus: jsTree visual glitch on hover
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13192'>OFBIZ-13192</a>] - Issues when uploading SVG files
+</li>
+</ul>
+
+<h2>Bug</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12296'>OFBIZ-12296</a>] - rest/control/main generates a 404
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13143'>OFBIZ-13143</a>] - Webtools FindGeneric, sort field is break on list
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13149'>OFBIZ-13149</a>] - Example charts don&#39;t work
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13150'>OFBIZ-13150</a>] - example/control/ManagePortalPages?parentPortalPageId=EXAMPLE works but reports an error
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13169'>OFBIZ-13169</a>] - Rainbowstone/Helveticus: advanced search cannot be open in modals
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13191'>OFBIZ-13191</a>] - Bug persisting uploaded files with service attachUploadToDataResource
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13210'>OFBIZ-13210</a>] - Registration of an user in ecommerce throws an error
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13211'>OFBIZ-13211</a>] - OFBiz Setup failing to setup the product store
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13218'>OFBIZ-13218</a>] - Correct the typo of entity name in cancelReplacementOrderItems method
+</li>
+</ul>
+
+<h2>New Feature</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13198'>OFBIZ-13198</a>] - Add the OpenSSF Best Practices Badge
+</li>
+</ul>
+
+<h2>Improvement</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-12821'>OFBIZ-12821</a>] - Improvement of the Check xml tag
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13133'>OFBIZ-13133</a>] - Allow to use GroovyDsl in FlexibleStringExpander
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13146'>OFBIZ-13146</a>] - Slimdown use of ${groovy: }
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13173'>OFBIZ-13173</a>] - Add display result format for auto-completion on lookup
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13178'>OFBIZ-13178</a>] - Update end bracket detection en groovy scriplet
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13179'>OFBIZ-13179</a>] - Improve ViewHandler interface
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13182'>OFBIZ-13182</a>] - Optimization on removeJob service
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13185'>OFBIZ-13185</a>] - Bug in Log4j
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13196'>OFBIZ-13196</a>] - Upgrade to 2.3.34 FreeMarker release
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13205'>OFBIZ-13205</a>] - ProjectServices: Project creation with template id should call &quot;createProject&quot; instead of &quot;createProjet&quot;
+</li>
+</ul>
+
+<h2<Task</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13201'>OFBIZ-13201</a>] - Migrate Build Scan publication to develocity.apache.org
+</li>
+</ul>
+
+</div>
+</div>
+</div>
+</div>
+</section>
+</section>
+
