697bc15bc03424e150264ca3b75032c1c3c01366
Nicolas Malin <nicolas.malin@nereide.fr>
2025-08-04 11:57:27 +0200

    Improved: Add new OFBiz version 24.09.02

---
+++ template/page/release-notes-24.09.02.tpl.php
@@ -0,0 +1,66 @@
+<?php //Variable declarations for region templates
+  $head_title = '<title>The Apache OFBiz&reg; Project - Release Notes 24.09.02</title>';
+?>
+ <!-- page content -->
+ <section id="content" class="fullWidth">
+    <header class="headerPage">
+      <div class="container clearfix">
+        <div class="row">
+          <h1 class="span8">Release Notes 24.09.02</h1>
+           <div class="span4" id="navTrail">
+               <a href="index.html" class="homeLink">home</a>
+               <span>/</span>
+               <a href="download.html">Download</a>
+               <span>/</span><span class="current">Release Notes 24.09.02</span>
+           </div>
+      </div>
+      </div>
+    </header>
+  <section id="content" class="features" >
+  <div class="slice clearfix">
+      <div class="container">
+<div class="row">
+  <div>
+  <p>Apache OFBiz&reg; 24.09.02, released in August 2025, is the second release of the 24.09 series, which has been feature-frozen since September 2024, receiving only bug fixes.</p>
+
+<h2>Sub-task</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13264'>OFBIZ-13264</a>] -         [SECURITY]  Several CVEs in Apache Tomcat</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13275'>OFBIZ-13275</a>] -         [SECURITY]  Several CVEs in Apache Tomcat</li>
+</ul>
+
+<h2>Bug</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13222'>OFBIZ-13222</a>] -         Error viewing entities with fromDate key in entity maintenance
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13223'>OFBIZ-13223</a>] -         Error rendering included form &quot;MandatoryWorkEfforts&quot; when approving parent production run
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13225'>OFBIZ-13225</a>] -         ClassCastException on PO Receipt in OFBiz - GStringImpl cannot be cast to String
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13226'>OFBIZ-13226</a>] -         Error When Adding Actual Material After Confirming Production Run in OFBiz
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13229'>OFBIZ-13229</a>] -         [SECURITY]  Several CVEs in Apache Tomcat
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13231'>OFBIZ-13231</a>] -         Ajax request fail on restful page
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13254'>OFBIZ-13254</a>] -         ArithmeticException when producing inventory with general cost set on routing task
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13258'>OFBIZ-13258</a>] -         Update communication event failed if statusId is null
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13265'>OFBIZ-13265</a>] -         Update Apache commons-fileupload to last version (CVE-2025-48976)
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13268'>OFBIZ-13268</a>] -         Fix ClassCastException in EntityConditionBuilder.createNode when using EntityFunction keys
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13270'>OFBIZ-13270</a>] -         Incorrect service call for internal requirement may trigger unnecessary production runs
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13274'>OFBIZ-13274</a>] -         Viewing records in Webtools/Entity Data Management got broken
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13279'>OFBIZ-13279</a>] -         Bugfix-ScreenFopViewHandler-check-adding-PDFEncryption
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13224'>OFBIZ-13224</a>] -         Support visual-editor-buttons attribute on textareas
+</li>
+<!--
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13276'>OFBIZ-13276</a>] -         [SECURITY] CVE-2025-54466 RCE Vulnerability in scrum plugin
+</li>
+-->
+</ul>
