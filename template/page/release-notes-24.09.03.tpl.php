52268c918d0187c7656be5832d43ade4e00950e5
Nicolas Malin <nicolas.malin@nereide.fr>
2025-11-10 10:04:47 +0100

    Improved: Add new OFBiz version 24.09.03

---
+++ template/page/release-notes-24.09.03.tpl.php
@@ -0,0 +1,63 @@
+<?php //Variable declarations for region templates
+  $head_title = '<title>The Apache OFBiz&reg; Project - Release Notes 24.09.03</title>';
+?>
+ <!-- page content -->
+ <section id="content" class="fullWidth">
+    <header class="headerPage">
+      <div class="container clearfix">
+        <div class="row">
+          <h1 class="span8">Release Notes 24.09.03</h1>
+           <div class="span4" id="navTrail">
+               <a href="index.html" class="homeLink">home</a>
+               <span>/</span>
+               <a href="download.html">Download</a>
+               <span>/</span><span class="current">Release Notes 24.09.03</span>
+           </div>
+      </div>
+      </div>
+    </header>
+  <section id="content" class="features" >
+  <div class="slice clearfix">
+      <div class="container">
+<div class="row">
+  <div>
+  <p>Apache OFBiz&reg; 24.09.03, released in November 2025, is the third release of the 24.09 series, which has been feature-frozen since September 2024, receiving only bug fixes.</p>
+
+      <h2>Sub-task
+      </h2>
+      <ul>
+          <li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13285'>OFBIZ-13285</a>] - [SECURITY]  Several CVEs in Apache Tomcat
+          </li>
+      </ul>
+
+      <h2>Bug
+      </h2>
+      <ul>
+          <li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13278'>OFBIZ-13278</a>] - Repeated REST API exception results in generic 500 error instead of specific response
+          </li>
+          <li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13284'>OFBIZ-13284</a>] - Set default security headers in writeJSONtoResponse and ensure SameSite cookie is returned
+          </li>
+          <li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13290'>OFBIZ-13290</a>] - InventoryReports screen broken
+          </li>
+          <li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13303'>OFBIZ-13303</a>] - Error map is missing in the returned service result
+          </li>
+          <li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13308'>OFBIZ-13308</a>] - OFBiz entity import screen is broken
+          </li>
+          <li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13310'>OFBIZ-13310</a>] - [SECURITY] Several CVEs in Apache Tomcat
+          </li>
+      </ul>
+
+      <h2>Improvement
+      </h2>
+      <ul>
+          <li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13292'>OFBIZ-13292</a>] - Improve ImageManagementServices code
+          </li>
+          <li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13293'>OFBIZ-13293</a>] - Reco
+          </li>
+          <li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13295'>OFBIZ-13295</a>] - Check parameters passed in URLs
+          </li>
+          <li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13304'>OFBIZ-13304</a>] - Improves init-gradle-wrapper.sh
+          </li>
+          <li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13309'>OFBIZ-13309</a>] - Hide Server Version Information on Error Pages
+          </li>
+      </ul>
