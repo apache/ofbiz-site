9c8153c0902ba9be7bbb8f72d50504ed84ac3b33
Nicolas Malin <nicolas.malin@nereide.fr>
2026-01-19 09:51:25 +0100

    Improved: Add new OFBiz version 24.09.05

---
+++ template/page/release-notes-24.09.05.tpl.php
@@ -0,0 +1,72 @@
+<?php //Variable declarations for region templates
+  $head_title = '<title>The Apache OFBiz&reg; Project - Release Notes 24.09.05</title>';
+?>
+ <!-- page content -->
+ <section id="content" class="fullWidth">
+    <header class="headerPage">
+      <div class="container clearfix">
+        <div class="row">
+          <h1 class="span8">Release Notes 24.09.05</h1>
+           <div class="span4" id="navTrail">
+               <a href="index.html" class="homeLink">home</a>
+               <span>/</span>
+               <a href="download.html">Download</a>
+               <span>/</span><span class="current">Release Notes 24.09.05</span>
+           </div>
+      </div>
+      </div>
+    </header>
+  <section id="content" class="features" >
+  <div class="slice clearfix">
+      <div class="container">
+<div class="row">
+  <div>
+  <p>Apache OFBiz&reg; 24.09.05, released in January 2026, is the fifth release of the 24.09 series, which has been feature-frozen since September 2024, receiving only bug fixes.</p>
+
+
+        Release Notes - OFBiz - Version 24.09.05
+<h2>        Bug
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13070'>OFBIZ-13070</a>] -         BalanceSheet generates an error
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13283'>OFBIZ-13283</a>] -         Canceling a partially received Purchase Order receipt throws a &quot;Required Field Missing: Receipt Id&quot; error.
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13317'>OFBIZ-13317</a>] -         Tried to execute unauthorized script
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13320'>OFBIZ-13320</a>] -         In several locations strings are compared with == and !=
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13321'>OFBIZ-13321</a>] -         getClass() is called on instance of Class
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13323'>OFBIZ-13323</a>] -         service copyInvoice Failed on invoice not in process
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13325'>OFBIZ-13325</a>] -         Invalid characters encountered
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13327'>OFBIZ-13327</a>] -         Fix various bugs in receive against purchase orders
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13330'>OFBIZ-13330</a>] -         Compound&#39;s single form are loaded as ModelGrid in memory
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13332'>OFBIZ-13332</a>] -         Lookup don&#39;t work on area just updated
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13333'>OFBIZ-13333</a>] -         Bad request parameter encoding with tomcat 9
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13335'>OFBIZ-13335</a>] -         Purchase Order approval fails when payment auto create config is disabled
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13337'>OFBIZ-13337</a>] -         &#39;New Shipment For Ship Group&#39; buttons should be available only after POs have been approved
+</li>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13339'>OFBIZ-13339</a>] -         jsgantt-improved bloks qs.js update
+</li>
+</ul>
+
+<h2>        Improvement
+</h2>
+<ul>
+<li>[<a href='https://issues.apache.org/jira/browse/OFBIZ-13257'>OFBIZ-13257</a>] -         The &quot;Select&quot; button in the Calendar Exception Day section functions as an edit action but lacks appropriate context and UI feedback, leading to user confusion
+</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</section>
+</section>
